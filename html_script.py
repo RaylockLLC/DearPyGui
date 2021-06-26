0f8d764045d65c0d9acdc405694bff261c91ff3a
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-06-26 02:36:00 -0500

    Update html_script.py

--- html_script.py
+++ html_script.py
@@ -2,9 +2,10 @@ from datetime import date
 
 # this file is for generating the docs from the simple and core html files that come from pdoc3
 
-# SIMPLE
+###########################################
+# DEARPYGUI
 # opening the simple file to scan for where to insert the logo
-with open('html\dearpygui\simple.html', 'r') as f:
+with open('html\dearpygui\dearpygui.html', 'r') as f:
     api_html = f.readlines()
 for index, line in enumerate(api_html):
     if line == '<body>\n':
@@ -26,9 +27,63 @@ api_html.pop(index)
 api_html.insert(index, '<li><code><a title="dearpygui" href="index.html">dearpygui</a></code></li>\n')
 
 # writing the final api docs with logo
-with open('api_simple.html', 'w') as f:
+with open('api_dearpygui.html', 'w') as f:
     f.writelines(api_html)
+############################################################
+# LOGGER
+# opening the simple file to scan for where to insert the logo
+with open('html\dearpygui\logger.html', 'r') as f:
+    api_html = f.readlines()
+for index, line in enumerate(api_html):
+    if line == '<body>\n':
+        break
 
+# inserting the logo and horizontal line
+api_html.insert(index, '<hr>\n')
+logo_html = ['<header>\n', '<h1><a href="index.html">Dear PyGui</a></h1>\n', '</header>\n']
+for count in logo_html:
+    api_html.insert(index, logo_html.pop())
+
+# scan again this time for the place to insert the link back to the main api_docs page
+for index, line in enumerate(api_html):
+    if 'title="dearpygui"' in line:
+        break
+
+# removing the old link and inserting the new inserting the new link
+api_html.pop(index)
+api_html.insert(index, '<li><code><a title="dearpygui" href="index.html">dearpygui</a></code></li>\n')
+
+# writing the final api docs with logo
+with open('api_logger.html', 'w') as f:
+    f.writelines(api_html)
+############################################################
+# DEMO
+# opening the simple file to scan for where to insert the logo
+with open('html\dearpygui\demo.html', 'r') as f:
+    api_html = f.readlines()
+for index, line in enumerate(api_html):
+    if line == '<body>\n':
+        break
+
+# inserting the logo and horizontal line
+api_html.insert(index, '<hr>\n')
+logo_html = ['<header>\n', '<h1><a href="index.html">Dear PyGui</a></h1>\n', '</header>\n']
+for count in logo_html:
+    api_html.insert(index, logo_html.pop())
+
+# scan again this time for the place to insert the link back to the main api_docs page
+for index, line in enumerate(api_html):
+    if 'title="dearpygui"' in line:
+        break
+
+# removing the old link and inserting the new inserting the new link
+api_html.pop(index)
+api_html.insert(index, '<li><code><a title="dearpygui" href="index.html">dearpygui</a></code></li>\n')
+
+# writing the final api docs with logo
+with open('api_demo.html', 'w') as f:
+    f.writelines(api_html)
+############################################################
 # CORE
 # opening the core file to scan for where to insert the logo
 with open('html\dearpygui\core.html', 'r') as f:
